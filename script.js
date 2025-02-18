1232e306c310e4bc7d2ff02ec99f5e1158396bb8
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-02-18 17:54:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumLatest =
+  "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
+
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
+  const minutes = Math.floor((currentTime - lastPost) / 60000);
+  const hours = Math.floor((currentTime - lastPost) / 3600000);
+  const days = Math.floor((currentTime - lastPost) / 86400000);
+  if (minutes < 60) {
+    return `${minutes}m ago`;
+  } else if (hours < 24) {
+    return `${hours}h ago`;
+  } else {
+    return `${days}d ago`;
+  }
+};
 
 const fetchData = async () => {
   try {
@@ -21,23 +37,21 @@ const showLatestPosts = (data) => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
 
-  postsContainer.innerHTML = topics.map((item) => {
-    const {
-      id,
-      title,
-      views,
-      posts_count,
-      slug,
-      posters,
-      category_id,
-      bumped_at,
-    } = item;
+  postsContainer.innerHTML = topics
+    .map((item) => {
+      const { id, title, views, posts_count, slug, posters, _id, bumped_at } =
+        item;
 
-    return `
-      <tr>
-      
-      </tr>
-    `;
-  }).join("");
+      return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
+      <td></td>
+    </tr>`;
+    })
+    .join("");
 };
-
